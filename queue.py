05fe25f5048e2f53b8f3946d7ef0717ea99e7ab9
Nikhil Sharma <nikhilsharmalku@gmail.com>
2021-07-28 09:37:07 +0530

    as

--- queue.py
+++ queue.py
@@ -1,44 +1,18 @@
-def underFlow(queue): #function to print if the queue is empty
-    if underFLOW == 1:
-        print("queue is underflow")
+def flow(queue): #function to print if the queue is empty
+    if length(queue) == 0:   #assigning the value of under flow
+        return "queue is underflow"
     else:
         return False
 
-def push(input): #function to enter an element
+def queue(queue, input): #function to enter an element
     queue.insert(0, input)
 
-def pop(queue): #function to remove an element
-    queue.pop(lenqueue-1)
+def dequeue(queue): #function to remove an element
+    queue.pop(length(queue)-1)
 
-def peek():    #function to display the element
+def peek(queue):    #function to display the element
     print(queue)
 
-queue = [1,2,3,4,5,6,17]    #specifying the queue
-lenqueue = len(queue)   #length of queue
-
-peek()  #calling peek function
-
-if lenqueue == 0:   #assigning the value of under flow
-    underFLOW = 1
-else:
-    underFLOW = 0
-
-Input = input("Command: ")  #input from user to perform further function
-
-if Input == "pop":  #checking the user input for pop
-    if underFLOW == 1:  #checking for underflow
-        print("queue is Empty")
-    else:
-        pop(queue)  #calling pop function
-
-elif Input == "push":   #checking for user input for push
-    pushValue = int(input("Enter a value to push: "))   #storing user input for pushh function
-    push(pushValue)    #calling push function
-
-elif Input == "peek":   #checkin user input for peek
-    peek(queue)
-
-else:   #checking user input for any other input
-    pass
-
-peek()
+def length(queue):
+    lenqueue = len(queue)   #length of queue
+    return lenqueue
---
+++ try.py
@@ -0,0 +1,9 @@
+import queue as q
+
+arr = [1]
+
+q.dequeue(arr)
+q.peek(arr)
+
+print(q.length(arr))
+print(q.flow(arr))
